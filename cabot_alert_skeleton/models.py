ad724a873c8662338580895b22a31a8bec70f657
Swapnil Palash <swapnil.palash@codenation.co.in>
2017-11-09 19:55:19 +0530

    Mocked funtionality testing

--- cabot_alert_skeleton/models.py
+++ cabot_alert_skeleton/models.py
@@ -5,8 +5,26 @@ from os import environ as env
 from logging import getLogger
 logger = getLogger(__name__)
 
+cachet_template = """
+Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %}*is back to normal*{% else %}reporting *{{ service.overall_status }}* status{% endif %}: {{ scheme }}://{{ host }}{% url 'service' pk=service.id %} \
+{% if alert %}{% for alias in users %} @{{ alias }}{% endfor %}{% endif %}\
+{% if service.overall_status != service.PASSING_STATUS %}Checks failing:\
+{% for check in service.all_failing_checks %}\
+    {% if check.check_category == 'Jenkins check' %}\
+        {% if check.last_result.error %}\
+            - {{ check.name }} ({{ check.last_result.error|safe }}) {{check.jenkins_config.jenkins_api}}job/{{ check.name }}/{{ check.last_result.job_number }}/console
+        {% else %}\
+            - {{ check.name }} {{check.jenkins_config.jenkins_api}}/job/{{ check.name }}/{{check.last_result.job_number}}/console
+        {% endif %}\
+    {% else %}
+        - {{ check.name }} {% if check.last_result.error %} ({{ check.last_result.error|safe }}){% endif %}
+    {% endif %}\
+{% endfor %}\
+{% endif %}\
+"""
+
 class SkeletonAlertUserData(AlertPluginUserData):
-    name = "Skeleton Plugin"
+    name = "Cachet Plugin"
     favorite_bone = models.CharField(max_length=50, blank=True)
 
 class SkeletonAlert(AlertPlugin):
@@ -14,11 +32,31 @@ class SkeletonAlert(AlertPlugin):
     author = "Jack Skellington"
 
     def send_alert(self, service, users, duty_officers):
-        calcium_level = env.get('CALCIUM_LEVEL')
-        message = service.get_status_message()
-        for u in users:
-            logger.info('{} - This is bad for your {}.'.format(
-                message,
-                u.cabot_alert_skeleton_settings.favorite_bone))
+        alert = True
+        users = list(users) + list(duty_officers)
+        if service.overall_status == service.WARNING_STATUS:
+            alert = False  # Don't alert at all for WARNING
+        if service.overall_status == service.ERROR_STATUS:
+            if service.old_overall_status in (service.ERROR_STATUS, service.ERROR_STATUS):
+                alert = False  # Don't alert repeatedly for ERROR
+        if service.overall_status == service.PASSING_STATUS:
+            color = 'good'
+            if service.old_overall_status == service.WARNING_STATUS:
+                alert = False  # Don't alert for recovery from WARNING status
+        else:
+            color = 'danger'
+
+        c = Context({
+            'service': service,
+            'users': users,
+            'host': settings.WWW_HTTP_HOST,
+            'scheme': settings.WWW_SCHEME,
+            'alert': alert,
+        })
+        message = Template(slack_template).render(c)
+        if alert:
+            self._send_cabot_alert(message, service, color=color, sender='Cabot')
 
         return True
+    def _send_cabot_alert(message, service,color, sender):
+        logger.info(message)
